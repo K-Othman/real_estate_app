cae3fb3f8c67d2d4293291be4deafc4ecf7d725f
K-Othman <karim_othman88@hotmail.com>
2024-04-23 00:51:42 +0100

    testing

--- app.js
+++ app.js
@@ -2,6 +2,10 @@ import express from "express";
 
 const app = express();
 
+app.use("/api/test", (req, res) => {
+  res.send("it is Working");
+});
+
 app.listen(8800, () => {
   console.log("Server Is Running!");
 });
